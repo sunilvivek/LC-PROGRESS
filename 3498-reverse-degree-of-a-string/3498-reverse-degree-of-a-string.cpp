3eb264de4c12532111b1b9927e80048df3a276d2
sunilvivek <svivekjain.2006@gmail.com>
2026-09-20 21:50:09 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int reverseValue = 26 - (s[i] - 'a');
+            ans += reverseValue * (i + 1);
+        }
+
+        return ans;
+    }
+};
